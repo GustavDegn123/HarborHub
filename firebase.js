a19a7cb31ad4fdbfb5af1b257931466614409325
Gustav Degn <gude23ab@student.cbs.dk>
2025-09-16 22:04:25 +0200

    oo

--- firebase.js
+++ firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCyiOE2Wa5FJYy7mwt3yTdaMPGQI-qIcqg",
   authDomain: "harborhub-f15de.firebaseapp.com",
   projectId: "harborhub-f15de",
-  storageBucket: "harborhub-f15de.appspot.com", // 🔥 ændret: skal slutte med .appspot.com
+  storageBucket: "harborhub-f15de.appspot.com",
   messagingSenderId: "16622525056",
   appId: "1:16622525056:web:cc30d1c66367b08cfa2465",
   measurementId: "G-S53BY4B59T"
